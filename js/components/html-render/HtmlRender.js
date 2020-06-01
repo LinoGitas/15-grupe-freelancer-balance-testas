05904aa8a79bb393999b959122a421556ca379c7
LinoGitas <63246130+LinoGitas@users.noreply.github.com>
2020-06-01 13:22:09 +0300

    initial render

---
+++ js/components/html-render/HtmlRender.js
@@ -0,0 +1,30 @@
+class HtmlRender {
+    constructor( container, data, htmlSection ) {
+        this.container = container;
+        this.data = data;
+        this.htmlSection = htmlSection;
+        this.init();
+    }
+
+    init() {
+        this.render();
+    }
+
+    render() {
+        const HTML = `
+        <div class="col-2">
+            <div class="info-left">
+                <h3>${this.data.h3}</h3>
+                <p>${this.data.date}</p>
+            </div>
+            <div class="info-right ${this.data.class}"> 
+                <i class="fa fa-check"></i>             
+                <h2>${this.data.title}</h2>
+                <p>${this.data.text}</p>
+            </div>
+        </div>`;
+        this.container.insertAdjacentHTML('beforeend', HTML);
+    }
+}
+
+export default HtmlRender;
